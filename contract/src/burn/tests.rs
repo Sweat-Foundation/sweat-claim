85c01c5561ef971d91a37d478d0a90d6d4c95ab6
Vasily Styagov <styagov.dev@gmail.com>
2024-04-19 18:21:49 +0100

    test: add tests for burn status

--- contract/src/burn/tests.rs
+++ contract/src/burn/tests.rs
@@ -1,6 +1,9 @@
 #![cfg(test)]
 
-use claim_model::api::{BurnApi, ClaimApi, RecordApi};
+use claim_model::{
+    api::{BurnApi, ClaimApi, RecordApi},
+    UnixTimestamp,
+};
 use near_sdk::{json_types::U128, PromiseOrValue};
 
 use crate::{
@@ -106,4 +109,75 @@ fn test_burn_when_outdated_tokens_don_not_exist() {
     assert_eq!(0, burnt_amount);
 
     assert!(!contract.is_service_call_running);
+}
+
+#[test]
+fn test_burn_status_legacy() {
+    let (mut context, mut contract, accounts) = Context::init_with_oracle();
+    let alice_id = accounts.alice;
+
+    context.switch_account(&accounts.oracle);
+
+    let mut current_timestamp = 360;
+
+    let _top_up_timestamp_1 = current_timestamp;
+    context.set_block_timestamp_in_seconds(current_timestamp);
+    contract.record_batch_for_hold_legacy(vec![(alice_id.clone(), U128(1_000_000_000))]);
+
+    current_timestamp += (contract.claim_period + 10) as u64;
+    context.set_block_timestamp_in_seconds(current_timestamp);
+
+    context.switch_account(&alice_id);
+    let claim_timestamp = current_timestamp;
+    // contract.claim();
+    // mime legacy claim
+    let account = contract.accounts_legacy.get_mut(&alice_id).unwrap();
+    account.claim_period_refreshed_at = claim_timestamp as _;
+    account.accruals.clear();
+    // ---
+
+    context.switch_account(&accounts.oracle);
+
+    current_timestamp += 3600;
+    let top_up_timestamp_2 = current_timestamp;
+    context.set_block_timestamp_in_seconds(top_up_timestamp_2);
+    contract.record_batch_for_hold_legacy(vec![(alice_id.clone(), U128(500_000_000))]);
+
+    let burn_status = contract.get_burn_status(alice_id.clone());
+    assert_eq!(Some(top_up_timestamp_2 as UnixTimestamp), burn_status.min_claimable_ts);
+    assert_eq!(claim_timestamp as UnixTimestamp, burn_status.claim_period_refreshed_at);
+    assert_eq!(contract.burn_period, burn_status.burn_period);
+}
+
+#[test]
+fn test_burn_status() {
+    let (mut context, mut contract, accounts) = Context::init_with_oracle();
+    let alice_id = accounts.alice;
+
+    context.switch_account(&accounts.oracle);
+
+    let mut current_timestamp = 360;
+
+    let _top_up_timestamp_1 = current_timestamp;
+    context.set_block_timestamp_in_seconds(current_timestamp);
+    contract.record_batch_for_hold(vec![(alice_id.clone(), U128(1_000_000_000))]);
+
+    current_timestamp += (contract.claim_period + 10) as u64;
+    context.set_block_timestamp_in_seconds(current_timestamp);
+
+    context.switch_account(&alice_id);
+    let claim_timestamp = current_timestamp;
+    contract.claim();
+
+    context.switch_account(&accounts.oracle);
+
+    current_timestamp += 3600;
+    let top_up_timestamp_2 = current_timestamp;
+    context.set_block_timestamp_in_seconds(top_up_timestamp_2);
+    contract.record_batch_for_hold(vec![(alice_id.clone(), U128(500_000_000))]);
+
+    let burn_status = contract.get_burn_status(alice_id.clone());
+    assert_eq!(None, burn_status.min_claimable_ts);
+    assert_eq!(claim_timestamp as UnixTimestamp, burn_status.claim_period_refreshed_at);
+    assert_eq!(contract.burn_period, burn_status.burn_period);
 }
