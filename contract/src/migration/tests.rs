ae3504983eca50c3aabe07853b9560576a238c3a
Vasily Styagov <styagov.dev@gmail.com>
2024-04-23 13:49:21 +0100

    test: add more migration tests

--- contract/src/migration/tests.rs
+++ contract/src/migration/tests.rs
@@ -96,12 +96,12 @@ fn test_migration_with_burnt_sweat_and_not_evaporating_sweat() {
     current_timestamp += 172_800; // + 2 days
     context.set_block_timestamp_in_seconds(current_timestamp); // Now it's day 2
 
-    // Record second top-up – 2 $SWEAT
-    let alice_balance_2 = U128(4 * 10u128.pow(18)); // 2 $SWEAT
+    // Record second top-up – 4 $SWEAT
+    let alice_balance_2 = U128(4 * 10u128.pow(18)); // 4 $SWEAT
     contract.record_batch_for_hold_legacy(vec![(alice.clone(), alice_balance_2)]);
 
     // Time travel to some point in the future where alice_balance_1 evaporates
-    // Now is 4 days from the start, so add not lees than 6 days.
+    // Now is 2 days from the start, so add not lees than 8 days.
     current_timestamp += 691_200; // + 8 days
     context.set_block_timestamp_in_seconds(current_timestamp); // Now it's day 10
 
@@ -162,6 +162,74 @@ fn test_migration_with_burnt_sweat_and_not_evaporating_sweat() {
 
     let alice_current_balance = contract.get_claimable_balance_for_account(alice.clone());
     // Expect balance without any fees, as Alice claimed right before migration
-    let expected_balance = 1_000_000_000_000_000_000;
+    let expected_balance = alice_balance_3.0;
+    assert_eq!(expected_balance, alice_current_balance.0);
+}
+
+#[test]
+fn test_migration_with_no_burnt_sweat() {
+    let (mut context, mut contract, accounts) = Context::init_with_oracle();
+    let alice = &accounts.alice.clone();
+
+    context.switch_account(&accounts.oracle);
+
+    let burn_period = 864_000; // 10 days in seconds
+    contract.set_burn_period(burn_period);
+
+    let mut current_timestamp = 0;
+
+    // Record first top-up – 3 $SWEAT
+    let alice_balance_1 = U128(3 * 10u128.pow(18)); // 3 $SWEAT
+    contract.record_batch_for_hold_legacy(vec![(alice.clone(), alice_balance_1)]);
+
+    // Time travel to some point in the future where alice_balance_1 is still not evaporated.
+    current_timestamp += 172_800; // + 2 days
+    context.set_block_timestamp_in_seconds(current_timestamp); // Now it's day 2
+
+    // Record second top-up – 2 $SWEAT
+    let alice_balance_2 = U128(2 * 10u128.pow(18)); // 2 $SWEAT
+    contract.record_batch_for_hold_legacy(vec![(alice.clone(), alice_balance_2)]);
+
+    // Time travel to some point in the future where both top-ups still don't evaporate
+    // Now is 2 days from the start, so add not more than 8 days.
+    current_timestamp += 172_800; // + 2 days
+    context.set_block_timestamp_in_seconds(current_timestamp); // Now it's day 4
+
+    let alice_current_balance = contract.get_claimable_balance_for_account(alice.clone());
+    assert_eq!(alice_balance_1.0 + alice_balance_2.0, alice_current_balance.0);
+
+    // Migrate the Alice account
+    let alice_account_outdated = contract.accounts_legacy.get(&alice).cloned().unwrap();
+    contract.migrate_accounts(vec![alice.clone()]);
+
+    // Check that the account record moved from legacy collection to the new one
+    assert!(contract.accounts_legacy.get(alice).is_none());
+    assert!(contract.accounts.get(alice).is_some());
+
+    // Check that the balance is still the same
+    let alice_current_balance = contract.get_claimable_balance_for_account(alice.clone());
+    assert_eq!(alice_balance_1.0 + alice_balance_2.0, alice_current_balance.0);
+
+    let alice_account = contract.accounts.get(alice).unwrap().into_latest();
+    assert_eq!(
+        alice_account_outdated.claim_period_refreshed_at,
+        alice_account.burn_since
+    );
+    assert_eq!(
+        alice_account_outdated.claim_period_refreshed_at,
+        alice_account.claim_period_refreshed_at
+    );
+    assert_eq!(alice_balance_1.0 + alice_balance_2.0, alice_account.balance);
+    assert!(alice_account.is_enabled);
+    assert!(!alice_account.is_locked);
+
+    // Time travel to some point in the future withing burn period.
+    // As burn period hasn't passed, it shouldn't evaporate.
+    current_timestamp += 86_400; // + 1 days
+    context.set_block_timestamp_in_seconds(current_timestamp); // Now it's day 5
+
+    let alice_current_balance = contract.get_claimable_balance_for_account(alice.clone());
+    // Expect balance without any fees, as Alice claimed right before migration
+    let expected_balance = alice_balance_1.0 + alice_balance_2.0;
     assert_eq!(expected_balance, alice_current_balance.0);
 }
