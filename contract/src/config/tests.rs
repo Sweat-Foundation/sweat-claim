69ef99e7a015f6f3aeaaa9153326220317a736b1
Vasily Styagov <styagov.dev@gmail.com>
2024-04-25 16:05:48 +0100

    feat: add restrictions for claim and burn periods

--- contract/src/config/tests.rs
+++ contract/src/config/tests.rs
@@ -7,6 +7,7 @@ use crate::common::tests::Context;
 #[test]
 fn set_claim_period_by_oracle() {
     let (mut context, mut contract, accounts) = Context::init_with_oracle();
+    contract.burn_period = 10_000_000;
 
     let claim_period = 1_000_000;
     context.switch_account(&accounts.oracle);
@@ -19,6 +20,35 @@ fn set_claim_period_by_oracle() {
     contract.set_claim_period(claim_period);
 
     assert_eq!(claim_period, contract.claim_period);
+}
+
+#[test]
+#[should_panic(expected = "Claim period should be less than burn period")]
+fn set_invalid_claim_period() {
+    let (mut context, mut contract, accounts) = Context::init_with_oracle();
+    contract.burn_period = 10_000_000;
+
+    context.switch_account(&accounts.oracle);
+    contract.set_claim_period(20_000_000);
+}
+
+#[test]
+#[should_panic(expected = "Burn period should be greater than claim period")]
+fn set_invalid_burn_period() {
+    let (mut context, mut contract, accounts) = Context::init_with_oracle();
+    contract.claim_period = 5_000_000;
+
+    context.switch_account(&accounts.oracle);
+    contract.set_burn_period(1_000_000);
+}
+
+#[test]
+#[should_panic(expected = "Burn period should be greater than 0")]
+fn set_zero_burn_period() {
+    let (mut context, mut contract, accounts) = Context::init_with_oracle();
+
+    context.switch_account(&accounts.oracle);
+    contract.set_burn_period(0);
 }
 
 #[test]
